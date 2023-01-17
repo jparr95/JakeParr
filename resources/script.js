05b3f2cad6aa5a514ed1a2e241d9f8f72d24640b
jparr95 <113371574+jparr95@users.noreply.github.com>
2023-01-17 15:25:55 -0500

    Add files via upload

---
+++ resources/script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.hobby-btn');
+const output = document.querySelector('.hobby-output');
+const hobbyList = ['Kite Flying',
+'Coffee Connoisseur',
+'Working Out',
+'Knitting',
+'Crochet',
+'Tai Kwon Do',
+'Parkour',
+'NFC Tag Automation',
+'Mechanical Keyboards'
+]
+const randomNumber = () => {
+    return Math.floor(Math.random() * hobbyList.length);
+}
+const generateHobby = () => {
+    randomNumber();
+    let hobbySelected = hobbyList[randomNumber()];
+    output.innerText = `I think I'm going to try ${hobbySelected} next!`;
+}
+// btn.onclick = generateHobby;
+btn.addEventListener('click', generateHobby);
